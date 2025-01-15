16a8298ea4f477dba73ecd3c7c21b000dc4a8e00
wikky02H <109713162+wikky02H@users.noreply.github.com>
2025-01-15 16:31:47 +0530

    InitialCommit

--- Models/ContentDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PavoWeb.Models
-{
-    public class ContentDetail
-    {
-        public int ContentDetailId { get; set; }
-        public int? ContentDetailOrderNo { get; set; }
-        public string? ContentDetailName { get; set; }
-    }
-}
--- Models/SubscriptionDetail.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PavoWeb.Models
-{
-    public class SubscriptionDetail
-    {
-        public int DetailId { get; set; }
-        public string? DetailDescription { get; set; }
-        public int? DetailOrderNo { get; set; }
-    }
-}
